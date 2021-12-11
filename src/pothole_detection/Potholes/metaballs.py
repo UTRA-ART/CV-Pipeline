e20f8c8212185f03eba82c022be7a80a868dd356
Ammar Vora <ammarmust4@gmail.com>
2021-12-10 21:05:52 -0500

    Scripts to generate synthetic pothole data and train the YOLOv4 object detection model for pothole detection.

---
+++ src/pothole_detection/Potholes/metaballs.py
@@ -0,0 +1,32 @@
+from PIL import Image
+import math
+
+img = Image.open(
+    "C:\\Users\\ammar\\Documents\\CodingProjects\\ART\\CV-Pipeline\\YOLOv4\\PyTorch_YOLOv4-train\\inference\\Lane_Input_2076.png")
+
+width, height = img.size
+
+mns = [[100, 100], [50, 50]]
+sns = [100, 20]
+k = 2
+g = 1
+r = 1.8
+
+for x in range(width):
+    for y in range(height):
+        total = 0
+
+        for b in range(k):
+            try:
+                val = (sns[b] / ((math.sqrt((mns[b][0] - x) **
+                                            2 + (mns[b][1] - y)**2)) ** g))
+            except ZeroDivisionError:
+                val = r * 2
+
+            total += val
+
+        if total > r:
+            # print(x, y)
+            img.putpixel((x, y), (0, 0, 0))
+
+img.save("new.png")
---
+++ src/pothole_detection/Potholes/split.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+from random import randint
+
+TRAIN_NUM = 1635
+VAL_NUM = 467
+TEST_NUM = 233
+TOTAL = 2335 - 1
+
+count = 0
+
+for num, type in [(TRAIN_NUM, 'train'), (VAL_NUM, 'valid'), (TEST_NUM, 'test')]:
+    for i in range(num):
+        files = os.listdir('./dataset/data/')
+        print(len(files))
+        image_path = Path('./dataset/data/' + files[randint(0, TOTAL)])
+        label_path = Path('./dataset/label/' +
+                          os.path.splitext(image_path.name)[0] + '.txt')
+
+        out_img = f'./dataset/{type}/images/' + image_path.name
+        out_lbl = f'./dataset/{type}/labels/' + label_path.name
+
+        os.rename(image_path, out_img)
+        os.rename(label_path, out_lbl)
+        TOTAL = TOTAL - 1
