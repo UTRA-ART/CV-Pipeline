d25ecc9e6a05a0e96eaeb68f49970c890a136e17
David Chu <thedavidchu@gmail.com>
2022-05-16 21:55:41 -0400

    README.md and cleaning changes. Moved images to a separate directory.

--- src/ramp_detection/ramp_detection.py
+++ src/ramp_detection/ramp_detection.py
@@ -4,29 +4,29 @@ import cv2
 import numpy as np
 
 
-CALIBRATION_IMAGE = "calibration.png"
-TEST_IMAGE = "test.png"
-TOLERANCE = 5
+CALIB_IMG_PATH: str = "images/calibration.png"
+TEST_IMAGE_PATH: str = "images/test.png"
+TOLERANCE: int = 10
 
 
-def calibrate_green() -> Tuple[
+def calibrate_green(calib_img_path: str, tol: int = 5) -> Tuple[
     Tuple[int, int], Tuple[int, int], Tuple[int, int]
 ]:
-    img = cv2.imread(CALIBRATION_IMAGE)
+    img = cv2.imread(calib_img_path)
     img_hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
     green_patch = img_hls
 
     hue = green_patch[:, :, 0]
-    min_hue = np.min(hue) - TOLERANCE
-    max_hue = np.max(hue) + TOLERANCE
+    min_hue = np.min(hue) - tol
+    max_hue = np.max(hue) + tol
 
     lum = green_patch[:, :, 1]
-    min_lum = np.min(lum) - TOLERANCE
-    max_lum = np.max(lum) + TOLERANCE
+    min_lum = np.min(lum) - tol
+    max_lum = np.max(lum) + tol
 
     sat = green_patch[:, :, 2]
-    min_sat = np.min(sat) - TOLERANCE
-    max_sat = np.max(sat) + TOLERANCE
+    min_sat = np.min(sat) - tol
+    max_sat = np.max(sat) + tol
 
     return (min_hue, max_hue), (min_lum, max_lum), (min_sat, max_sat)
 
@@ -54,18 +54,18 @@ def get_ramp(
     return within_ramp.astype(np.uint8) * 255
 
 
-def test():
-    hue_range, lum_range, sat_range = calibrate_green()
-    img = cv2.imread(TEST_IMAGE)
+def main():
+    hue_range, lum_range, sat_range = calibrate_green(CALIB_IMG_PATH, TOLERANCE)
+    img = cv2.imread(TEST_IMAGE_PATH)
     img_hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+
     within_ramp = get_ramp(img_hls, hue_range, lum_range, sat_range)
-    return within_ramp
-
-
-if __name__ == "__main__":
-    within_ramp = test()
-
     cv2.imshow("Pixels Belonging to the Ramp", within_ramp)
+
     # End of program
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
