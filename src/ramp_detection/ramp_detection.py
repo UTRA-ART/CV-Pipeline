aa61a73ddd2b5b40382467f020be8b62019277b3
David Chu <thedavidchu@gmail.com>
2022-05-25 22:09:40 -0400

    Add description and bounding box filters

--- src/ramp_detection/ramp_detection.py
+++ src/ramp_detection/ramp_detection.py
@@ -1,12 +1,42 @@
+"""
+Ramp Detection Algorithm
+========================
+
+Description of Functionality
+----------------------------
+1. Sample the colour of the main body of the ramp. The image containing the
+sample is stored in $CALIB_IMG_PATH.
+2. Define a tolerance, within the bounds of which we accept that a pixel matches
+the shade of green of the ramp.
+3. Find all pixels in the image within the tolerance of green.
+4. With the image of all the matching "green" pixels, we want to cluster the
+shapes (I use contours because I am lazy).
+5. We take all contours larger than a given area.
+6. We approximate each contour with a convex hull and bound the contour with a
+rectangle
+7. For each of these combinations, we take the rectangles where the
+corresponding convex hull makes up at least $IOU_THRESH of the area.
+
+Immediately Foreseeable Limitations
+-----------------------------------
+1. To avoid accepting a contour that takes up the entirety of the image (e.g.
+the contours are "inside-out"), we specify a maximum area that the ramp can be.
+If we are very close to or on the ramp, then this may cause the model to stop
+detecting the ramp.
+2. We are so far unable to identify rotated-but-still-rectangular shapes.
+"""
+
 from typing import List, Tuple
 
 import cv2
 import numpy as np
 
 
-CALIB_IMG_PATH: str = "images/calibration.png"
-TEST_IMAGE_PATH: str = "images/test.png"
-TOLERANCE: int = 10
+SUBSTITUTE: str = "topview"
+CALIB_IMG_PATH: str = f"images/{SUBSTITUTE}_calibration.png"
+TEST_IMAGE_PATH: str = f"images/{SUBSTITUTE}.png"
+GREEN_TOLERANCE: int = 10
+IOU_THRESHOLD: float = 0.7
 
 
 def calibrate_green(
@@ -37,6 +67,20 @@ def get_green(
     lum_range: Tuple[int, int],
     sat_range: Tuple[int, int],
 ) -> np.ndarray:
+    """
+    Notes
+    -----
+    1. I tried this function with only hue, but it did not work (because of the
+    grass in the "training" images). Maybe try this again in competition? In its
+    current state, I do not believe there is any advantage over just comparing
+    to RGB (although I haven't tested this hypothesis). My reasoning, however,
+    is that both form a cube in 3-D of acceptable colours. Maybe the tolerance
+    causes it to behave differently though. I'm sure there's a mathematical way
+    to express the notion that I'm going at. That I think the two 3-D shapes
+    that we create are shapes that a linear transform can transform between? No,
+    I think more precisely, what is within the prism in one space remains in the
+    prism in the second space after the transform.
+    """
     hue = img_hls[:, :, 0]
     min_hue, max_hue = hue_range
     within_hue = np.logical_and(min_hue <= hue, hue <= max_hue)
@@ -57,18 +101,15 @@ def get_green(
 def get_large_contours(
     img_bgr: np.ndarray,
     min_area: float = 30.0,
+    max_area: float = float("inf"),
     c: int = 10,
     draw_contours: bool = False,
 ) -> List[np.ndarray]:
-    """
-    We want the
-    """
     # It may be useful to inspect the greyscale colour difference between the
     # ramp and the ground. Depending on which is lighter, the user may wish to
     # use either cv2.THRESH_BINARY (ramp is lighter??) or cv2.THRESH_BINARY_INV
     # (ramp is darker??).
     img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
-    print(img_gray.shape)
 
     # Debug this file by inspecting the thresh image. This can be done with
     # `cv2.imshow("Window Name", thresh)`. A useful reference guide can be found
@@ -77,7 +118,7 @@ def get_large_contours(
         img_gray,
         255,
         cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-        cv2.THRESH_BINARY,
+        cv2.THRESH_BINARY_INV,
         11,  # This _must_ be an odd number
         c,
     )
@@ -85,14 +126,15 @@ def get_large_contours(
         thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
     )
 
-    sorted_contours = sorted(contours, key=cv2.contourArea)
-
     # Sort by area
-    large_contours = [c for c in sorted_contours if cv2.contourArea(c) > min_area]
+    sorted_contours = sorted(contours, key=cv2.contourArea)
+    large_contours = [
+        c for c in sorted_contours if min_area < cv2.contourArea(c) < max_area
+    ]
 
     if draw_contours:
         img_bgr_copy = img_bgr.copy()
-        # Drop top 10 largest contours
+        # Draw top 10 largest contours
         cv2.drawContours(img_bgr_copy, sorted_contours[-10:], -1, (0, 255, 0), 1)
         # Draw contours larger than min_area
         cv2.drawContours(img_bgr_copy, large_contours, -1, (255, 0, 0), 1)
@@ -100,29 +142,68 @@ def get_large_contours(
             img_bgr_copy, large_contours, len(large_contours) - 1, (0, 0, 255), 1
         )
         cv2.imshow(
-            f"Large contours, {c=} (green=top-10, blue=larger than min_area, red=largest",
+            f"Large contours, {c=} "
+            + "(green=top-10, blue=larger than min_area, red=largest)",
             img_bgr_copy,
         )
 
     return large_contours
 
 
-def main():
-    hue_range, lum_range, sat_range = calibrate_green(CALIB_IMG_PATH, TOLERANCE)
+def main(draw_contours: bool = False):
+    hue_range, lum_range, sat_range = calibrate_green(CALIB_IMG_PATH, GREEN_TOLERANCE)
     img = cv2.imread(TEST_IMAGE_PATH)
 
-    # Green Image
+    # Collect pixels that match green
     img_hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-    within_ramp = get_green(img_hls, hue_range, lum_range, sat_range)
-    cv2.imshow("Pixels Belonging to the Ramp", within_ramp)
+    matches_green = get_green(img_hls, hue_range, lum_range, sat_range)
 
-    within_ramp_bgr = np.stack([within_ramp, within_ramp, within_ramp], axis=2)
-    contours = get_large_contours(within_ramp_bgr, 1000, draw_contours=True)
+    # We are expecting a 3-D image, so we stack the 2-D tensors
+    matches_green_bgr = np.stack([matches_green, matches_green, matches_green], axis=2)
+    height, width, _ = matches_green_bgr.shape
+    contours = get_large_contours(
+        matches_green_bgr,
+        min_area=1e3,
+        max_area=height * width / 2,  # Is this too tight of a bound?
+        draw_contours=draw_contours,
+    )
 
-    # End of program
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # Process the contours
+    # We take the convex hull to smooth out the divets in the contours and
+    # simplify the computation.
+    convex_hulls, bounding_rects, bounding_rot_rects = (
+        [cv2.convexHull(c) for c in contours],
+        [cv2.boundingRect(c) for c in contours],
+        [np.int0(cv2.boxPoints(cv2.minAreaRect(c))) for c in contours],
+    )
+    tight_bounding_rects, tight_bounding_rot_rects = [
+        r
+        for c, r in zip(convex_hulls, bounding_rects)
+        if cv2.contourArea(c) >= IOU_THRESHOLD * r[2] * r[3]
+    ], [
+        r
+        for c, r in zip(convex_hulls, bounding_rot_rects)
+        if cv2.contourArea(c) >= IOU_THRESHOLD * cv2.contourArea(r)
+    ]
+
+    if draw_contours:
+        cv2.imshow("Pixels Belonging to the Ramp", matches_green)
+
+        cv2.drawContours(matches_green_bgr, convex_hulls, -1, (0, 255, 0), 1)
+        for rect in tight_bounding_rects:
+            x, y, dx, dy = rect
+            cv2.rectangle(matches_green_bgr, (x, y), (x + dx, y + dy), (255, 0, 0), 1)
+        cv2.drawContours(
+            matches_green_bgr, tight_bounding_rot_rects, -1, (0, 0, 255), 1
+        )
+        cv2.imshow("Boxes containing parts of the ramp", matches_green_bgr)
+
+        # End of program
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    return tight_bounding_rects
 
 
 if __name__ == "__main__":
-    main()
+    main(draw_contours=True)
