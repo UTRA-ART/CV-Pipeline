14328630f118797f282be60f8b3c59ec378da532
Ammar Vora <ammarmust4@gmail.com>
2023-01-18 20:10:16 -0500

    Adding YOLOv4 to yolov4-pothole

---
+++ src/pothole_detection/yolov4-pothole/YOLOv4/PyTorch_YOLOv4-train/testDetect.py
@@ -0,0 +1,12 @@
+import os
+
+input_directory = "C:\\Users\\ammar\\Documents\\CodingProjects\\ART\\CV-Pipeline\\src\\pothole_detection\\YOLOv4\\PyTorch_YOLOv4-train\\inference\\test3"
+
+command = 'python detect.py --weights "best.pt" --output "inference/output_test2/" --source '
+
+for filename in os.listdir(input_directory):
+    f = os.path.join(input_directory, filename)
+
+    if os.path.isfile(f):
+        os.system(command + '"' + f + '"' + " --device 1 --save-txt")
+        print(command + '"' + f + '"' + " --device 1 --save-txt")
